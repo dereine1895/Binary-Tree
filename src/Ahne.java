9a9f2d7738fba243348c1a0ae77619082a8cb974
Tom <tom@mvjt.de>
2025-03-18 13:24:07 +0100

    klassen struktur

--- src/Ahne.java
+++ src/Ahne.java
@@ -1,14 +1,36 @@
 public class Ahne {
-    private String vorname, nachname;
+    private String vorname;
+    private String nachname;
     private char geschlecht;
 
     public void Ahne(String pVN, String pNN, char pG){
+       nachname = pNN;
+       vorname = pVN;
+       geschlecht = pG;
 
     }
-    public void getter{
 
+    public char getGeschlecht() {
+        return geschlecht;
     }
-    public void setter{
 
+    public void setGeschlecht(char geschlecht) {
+        this.geschlecht = geschlecht;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
     }
 }
