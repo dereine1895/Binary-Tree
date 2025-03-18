477304c3272328fbb470eef79222ec4e3aa6f06e
Tom <tom@mvjt.de>
2025-03-18 13:12:43 +0100

    klassen struktur

---
+++ src/Ahne.java
@@ -0,0 +1,14 @@
+public class Ahne {
+    private String vorname, nachname;
+    private char geschlecht;
+
+    public void Ahne(String pVN, String pNN, char pG){
+
+    }
+    public void getter{
+
+    }
+    public void setter{
+
+    }
+}
---
+++ src/Ahnenbaum.java
@@ -0,0 +1,6 @@
+public class Ahnenbaum {
+
+    public Ahnenbaum(){
+
+    }
+}
